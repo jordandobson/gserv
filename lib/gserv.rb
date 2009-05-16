eb4c1f19201d1e6668b55ebfea84c31155d15a9a
Jordan Dobson <jordan.dobson@madebysquad.com>
2009-05-15 17:42:27 -0700

    Added request to gserver

--- lib/gserv.rb
+++ lib/gserv.rb
@@ -3,9 +3,43 @@ require 'gserver'
 class Gserv < GServer 
 
   VERSION = '1.0.0'
+  CRLF = "\r\n"
+  
+  attr_accessor :path, :protocol, :headers  
+  
+  def initialize(port=12321, *args)
+    super
+  end
   
   def serve(io)
-    io.puts(Time.now.to_s)
+    req = http_in io
+    io.puts(req)
+  end
+  
+  def http_in io
+    req = ""
+    io.each_line do |l|
+      break if l == CRLF
+      req << l
+    end
+    puts "------"
+    puts req
+    puts "------"
+    parse req
+    "HTTP/1.1 200 OK"
+  end
+  
+  def parse req
+    @headers = {}
+    req.each_line do |l|
+      l.strip!
+      if l =~ /GET (\/[^\s]*) HTTP\/(\d.\d)/
+        @path       = $1
+        @protocol   = $2
+      elsif l =~ /(.+): (.+)/
+        @headers[$1] = $2
+      end
+    end
   end
 
 end
--- test/test_gserv.rb
+++ test/test_gserv.rb
@@ -1,17 +1,33 @@
+require "uri"
 require "test/unit"
 require "gserv"
+require "net/http"
 
 class TestGserv < Test::Unit::TestCase
 
   def setup
-    @port = 1234
+    @port = 12321
     @server = Gserv.new(@port)
+    
+  end
+  
+  def teardown
+    @server.stop
   end
   
   def test_start_and_stop_server
+    assert @server.stopped?
     @server.start
-    assert GServer.in_service?(@port)
+    assert !@server.stopped?
     @server.stop
+    assert @server.stopped?
   end
-
+  
+  def test_request
+    @server.start
+    res = Net::HTTP.get_response URI.parse("http://localhost:#{@port}")
+    assert_equal '/',   @server.path
+    assert_equal '1.1', @server.protocol
+  end
+ 
 end
